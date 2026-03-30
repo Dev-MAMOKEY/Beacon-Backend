4d57dd9aadddbf3f9072631470f552b4b6909d2c
이수종 <leesj9627@naver.com>
2026-03-30 19:52:40 +0900

    [Feat/#7]: dto구현

---
+++ src/main/java/com/mamoki/beacon/global/fcm/dto/FcmDto.java
@@ -0,0 +1,16 @@
+package com.mamoki.beacon.global.fcm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FcmDto {
+    private String title;
+    private String content;
+    private String fcmToken;
+}
