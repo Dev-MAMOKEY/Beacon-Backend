8c25ce5b97ae614069d75b9a31996855e644afe6
DESKTOP-E7S6P6O\u <leesj9627@naver.com>
2026-04-04 02:16:48 +0900

    [Feat/#12]: startSession, updatedSession, softDeletedSession 함수 구현

--- src/main/java/com/mamoki/beacon/domain/session/service/SessionService.java
+++ src/main/java/com/mamoki/beacon/domain/session/service/SessionService.java
@@ -11,6 +11,7 @@ import com.mamoki.beacon.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
@@ -33,6 +34,48 @@ public class SessionService {
                 .endAt(sessionDto.getEndAt())
                 .club(club)
                 .build();
+        //세션이 ACTIVED이면 에러던짐
+        if(session.getSessionStatus() == SessionStatus.ACTIVED){
+            throw new CustomException(ErrorCode.SESSION_ALREADY_ACTIVE);
+        }
         sessionRepository.save(session);
     }
+
+    //softDelete 시간 업데이트
+    public void softDeletedSession(Long sessionId){
+        Session session = sessionRepository.findById(sessionId).orElseThrow(()-> new CustomException(ErrorCode.SESSION_NOT_FOUND));
+        session.setDeletedAt(LocalDateTime.now());
+        sessionRepository.save(session);
+    }
+
+    //session 업데이트 (입력 안한건 업데이트 안함), 주 목적은 세션 상태를 위한 함수
+    public void updatedSession(Long sessionId, SessionDto sessionDto){
+        Session session = sessionRepository.findById(sessionId)
+                .orElseThrow(()-> new CustomException(ErrorCode.SESSION_NOT_FOUND));
+
+        if(sessionDto.getSessionName() != null){
+            session.setSessionName(sessionDto.getSessionName());
+        }
+        if(sessionDto.getStartAt() != null){
+            session.setStartAt(sessionDto.getStartAt());
+        }
+        if(sessionDto.getEndAt() != null){
+            session.setEndAt(sessionDto.getEndAt());
+        }
+        if(sessionDto.getSessionStatus() != null){
+            session.setSessionStatus(sessionDto.getSessionStatus());
+        }
+
+        sessionRepository.save(session);
+    }
+
+    //session시작하고 ACTIVED로 변경하기 위한 세션시작 함수
+    public void startedSession(Long sessionId){
+        Session session = sessionRepository.findById(sessionId)
+                .orElseThrow(()-> new CustomException(ErrorCode.SESSION_NOT_FOUND));
+        session.setSessionStatus(SessionStatus.ACTIVED);
+        sessionRepository.save(session);
+    }
+
+    //TOTP 인증 코드 세션 테이블에 해시값으로 저장?
 }
