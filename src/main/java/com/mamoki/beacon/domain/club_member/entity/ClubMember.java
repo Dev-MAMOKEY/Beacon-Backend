1d1ff9d8cc9ce9ae83f2a177c1e2fd7464635199
kdh <soccerkdh12@naver.com>
2026-04-13 13:16:29 +0900

    [Fix/#20] Fix: 테스트 진행 중 오류 로직 수정

--- src/main/java/com/mamoki/beacon/domain/club_member/entity/ClubMember.java
+++ src/main/java/com/mamoki/beacon/domain/club_member/entity/ClubMember.java
@@ -16,18 +16,16 @@ import java.time.LocalDateTime;
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "club_member")
+@Table(name = "club_member", // 복합키로 유일성 보장하기 위해 uniqueConstraints 추가. 즉, 한 멤버가 같은 동아리 중복 가입 방지 위해 설정
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"member_id", "club_id"})
+    }
+)
 public class ClubMember extends GlobalEntity { // GlobalEntity를 상속해서 softDelete() 메서드 사용하기 위함
 
-    @EmbeddedId
-    private ClubMemberId id;
-
-    @MapsId("memberId")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @MapsId("clubId")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "club_id")
     private Club club;
@@ -40,7 +38,6 @@ public class ClubMember extends GlobalEntity { // GlobalEntity를 상속해서 s
     private LocalDateTime joinedAt;
 
     public ClubMember(Member member, Club club, Role role) {
-        this.id = new ClubMemberId(member.getId(), club.getId());
         this.member = member;
         this.club = club;
         this.role = role;
