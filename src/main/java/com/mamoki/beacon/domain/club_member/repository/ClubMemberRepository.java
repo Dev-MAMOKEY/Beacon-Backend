38224d9ee05785dacf81e005ca30ec997018d1a2
DESKTOP-E7S6P6O\u <leesj9627@naver.com>
2026-04-14 01:27:02 +0900

    [Feat/#12]: 세션 수정

--- src/main/java/com/mamoki/beacon/domain/club_member/repository/ClubMemberRepository.java
+++ src/main/java/com/mamoki/beacon/domain/club_member/repository/ClubMemberRepository.java
@@ -3,6 +3,7 @@ package com.mamoki.beacon.domain.club_member.repository;
 import com.mamoki.beacon.domain.club_member.entity.ClubMember;
 import com.mamoki.beacon.domain.club_member.entity.ClubMemberId;
 import com.mamoki.beacon.domain.member.entity.Member;
+import com.mamoki.beacon.domain.session.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,5 +13,5 @@ public interface ClubMemberRepository extends JpaRepository<ClubMember, ClubMemb
     // 특정 회원이 가입한 모든 ClubMember 조회 (Role 결정에 사용)
     List<ClubMember> findByMember(Member member);
     Optional<ClubMember> findByMemberIdAndClubId(Long memberId, Long clubId);
-
+    List<ClubMember> findByClubId(Long clubId);
 }
